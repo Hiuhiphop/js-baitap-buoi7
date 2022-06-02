5e91a0eefb8274b72dbff3500a1a2b2afc92f4ff
hieuhiphop <hieuhiphop797@gmail.com>
2022-06-03 00:21:12 +0700

    6/3'

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var count = [];
-for (var k = 1; k <= 10; k++) {
-  for (var i = k * 10 - 9; i <= k * 10; i++) {
-    [count += i];
+function ex1() {
+  var count = "";
+  for (var k = 1; k <= 10; k++) {
+    for (var i = k * 10 - 9; i <= k * 10; i++) {
+      count += i + " ";
+    }
+    count += "<br>";
   }
-    count += "\n";
+  document.getElementById("ex1").innerHTML = count;
 }
-console.log([count]);
+
+function ex2() {
+  //   debugger;
+  var str = document.getElementById("ex2").value;
+  var arr = str.split(" ");
+  console.log(arr);
+  var prime = "";
+  var flag = true;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] <= 2) {
+      flag = false;
+    } else {
+      for (var k = 2; k < arr[i] - 1; k++) {
+        if (arr[i] % k == 0) {
+          flag = false;
+          break;
+        } else {
+          prime = arr[i];
+        }
+      }
+    }
+  }
+  if (flag == true) {
+    console.log(prime, "là số nguyên");
+  }
+}
